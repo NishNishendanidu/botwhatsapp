e52df7053bb866f89f424a818d02c919ac765e37
NishNishendanidu <96036613+NishNishendanidu@users.noreply.github.com>
2022-02-20 13:21:37 +0530

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2021 Queen Amdi.
+ .
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Queen Amdi - Black Amda
+кιиg αм∂α-χ- nishendanidu 
 */
 
 const Amdi = require('../events');
