8a43a81e2f108e43cf0170282d48ff102ff24f7f
Shen bro <96036613+NishNishendanidu@users.noreply.github.com>
2022-02-20 22:36:37 +0530

    Update tblend.js

--- plugins/tblend.js
+++ plugins/tblend.js
@@ -1,12 +1,8 @@
-/* Copyright (C) 2021 Queen Amdi.
+ 
 
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-Queen Amdi - Black Amda
 */
 
-const Amdi = require('../events');
+const Amda= require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
