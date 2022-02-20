061c2e79e42af17c33d70cd8a4ae99424babf45f
NishNishendanidu <96036613+NishNishendanidu@users.noreply.github.com>
2022-02-20 14:04:36 +0530

    Update lyric.js

--- plugins/lyric.js
+++ plugins/lyric.js
@@ -2,7 +2,7 @@
 Copyright (C) 2021 Queen Amdi.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Queen Amdi - Black Amda
+кιиg αм∂α-χ-nishen danidu
 
 */
 
